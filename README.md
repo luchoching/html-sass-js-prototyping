da8935282dd4f08da3417b35baeadd4f5b8fa129
Lu Cho-Ching <choching.work@gmai.com>
2015-07-24 13:50:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+
+html + js + sass autorelaod --> for prototyping 
+
+use:  gulpjs + browsersync
+
+http://www.shakyshane.com/javascript/nodejs/2014/01/25/gulpjs-sass-browsersync-ftw/
+
+
+```
+// Assumed file/dir structure
+index.html
+css/
+scss/
+js/
+```
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+//compile scss into css
+gulp.task('sass', function(){
+  gulp
+    .src('scss/main.scss')
+    .pipe(sass({includePaths: ['scss']}))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('browser-sync', function(){
+  browserSync.init(['css/*.css', 'js/*.js'], {
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function(){
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('js/*.js').on('change', browserSync.reload);
+});
